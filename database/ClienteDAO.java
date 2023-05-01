bf8b4f1b8bef64bcc7c6109f60046f1f7b4fcf2c
João Pedro Cintra Karpinski <111706019+JoaoPKarpinski@users.noreply.github.com>
2023-05-01 01:42:45 -0400

    adicionando

--- database/ClienteDAO.java
+++ database/ClienteDAO.java
@@ -51,4 +51,65 @@ public class ClienteDAO {
 
         return qtdLinhasAfetadas;
     }
+
+
+    public void atualizar(Cliente cliente) {
+        try {
+
+            PreparedStatement statementInsercao = con.createStatement(
+                    "UPDATE Cliente SET cpf = ?, nome = ?, telefone = ?, email = ?, " +
+                    "dataNascimento = ?, rua = ?, numero = ?, bairro = ?, cidade = ?, uf = ?, " +
+                    "WHERE cpf = ?"
+            );
+
+            statementInsercao.setString(1, cliente.getCpf());
+            statementInsercao.setString(2, cliente.getNome());
+            statementInsercao.setString(3, cliente.getFone());
+            statementInsercao.setString(4, cliente.getEmail());
+            statementInsercao.setDate(5, cliente.getDataNascimento());
+            statementInsercao.setString(6, cliente.getRua());
+            statementInsercao.setInt(7, cliente.getNumero());
+            statementInsercao.setString(8, cliente.getBairro());
+            statementInsercao.setString(9,cliente.getCidade());
+            statementInsercao.setString(10, cliente.getUf());
+            statementInsercao.setString(11, cliente.getCpf());
+
+            statementInsercao.execute();
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao tentar atualizar usuário!");
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    	public static ArrayList<Produto> selectAll(){
+		ArrayList<Produto> arrayRes = new ArrayList<>();
+		Connection conexaoPadrao = new ConnectionFactory().getConexao(); 
+		try {
+			PreparedStatement prepSt = conexaoPadrao.prepareStatement("SELECT * FROM produtos");
+			ResultSet tuplasRes = prepSt.executeQuery(); 
+			while (tuplasRes.next()) {
+				arrayRes.add(new Produto(tuplasRes.getString("nome"), tuplasRes.getDouble("preco")));
+			}
+			ConnectionFactory.checkSearch(arrayRes);
+			return (arrayRes);
+		} catch (SQLException e) {
+			System.out.println("Ocorreu um erro na execução da query de consulta: " + e.getMessage());
+		} catch (EmptyQueryException e) {
+			System.out.println("Houve uma exceção: " + e.getMessage());
+		} finally {			
+			try {
+				conexaoPadrao.close();
+			} catch (SQLException e) {
+				System.out.println("Ocorreu uma exceção ao fechar a conexão: " + e.getMessage());
+			}
+		}
+		return arrayRes;
+	}
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
